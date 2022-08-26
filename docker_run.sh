52d85bad47e576bec609e4c464c2b3298a8135a9
Joonyeong97 <lyt970120@gmail.com>
2022-08-27 00:21:16 +0900

    update

--- docker_run.sh
+++ docker_run.sh
@@ -3,7 +3,7 @@
 docker run -it --gpus all -p 8209:8888 -v /mnt/d/notebook:/home/ezmeta/work \
 -v /mnt/d/data:/home/ezmeta/data \
 --name jupyter-lab \
-docker.io/joon09/jupyter-lab-gpu:ezmeta
+docker.io/joon09/jupyter-lab-gpu:ezmeta01
 
 
 #docker run -it --gpus all -p 8209:8888 \
